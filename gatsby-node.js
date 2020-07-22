7fc4721669fe8d2d8701ced529fb8f992fd32bce
Dennis Block <dennis.block@gmx.de>
2020-07-22 16:55:39 +0200

    Add Webpack config

---
+++ gatsby-node.js
@@ -0,0 +1,16 @@
+// https://www.gatsbyjs.org/docs/node-apis/#onCreateWebpackConfig
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    // https://www.gatsbyjs.org/docs/debugging-html-builds/#fixing-third-party-modules
+    if (stage === "build-html") {
+        actions.setWebpackConfig({
+            module: {
+                rules: [
+                    {
+                        test: /scrollreveal/,
+                        use: loaders.null(),
+                    },
+                ],
+            },
+        })
+    }
+}
